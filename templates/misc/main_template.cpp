d0a00bc6ffac97988fdd4ca55665ec8d05624c72
sanssanssanssanssanssans <ajona5427@kakao.com>
2026-04-18 22:45:32 +0900

    main_template

---
+++ templates/misc/main_template.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define fastio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+int main() {
+    fastio;
+}
